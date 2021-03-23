8ed4bc64014b66a9a366c1e7ae36d3dbe58e19c0
DanielMichael82 <danielmpearce@live.co.uk>
2021-03-23 12:58:45 +0000

    Added dropdown to the locations link on the navbar, this will assist easier navigation of the site. The dropdown includes a page for each of the counties.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,26 +1,26 @@
 function initMap() {
-        let map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: {lat: 50.7488, lng: -2.3445}
-        });
-        let geocoder = new google.maps.Geocoder();
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: { lat: 50.7488, lng: -2.3445 }
+    });
+    let geocoder = new google.maps.Geocoder();
 
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-      }
+    document.getElementById('submit').addEventListener('click', function () {
+        geocodeAddress(geocoder, map);
+    });
+}
 
-      function geocodeAddress(geocoder, resultsMap) {
-        let address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
+function geocodeAddress(geocoder, resultsMap) {
+    let address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
             let marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
+                map: resultsMap,
+                position: results[0].geometry.location
             });
-          } else {
+        } else {
             alert(`Location was not successful for the following reason: ${status}`);
-          }
-        });
-      }
+        }
+    });
+}
