fbbbcae9e736792185536126d4826078c9eafd58
DanielMichael82 <danielmpearce@live.co.uk>
2021-04-23 21:02:31 +0000

    Fixed issue with images not showing on the headers, also added more markers to the maps and more content for the infowindow.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,100 +4,115 @@ function initMap() {
         center: { lat: 51.1789, lng: -1.8262 },
     });
 
-    const marker1 = new google.maps.Marker({
-          position: new google.maps.LatLng(50.6212, -2.2768),
-          map: map,
-          title: 'Durdle Door'
-    });
-    
-    const infoWindow1 = new google.maps.InfoWindow({
-        content: '<div id="content">' +
+    // Array of markers
+    const markers = [
+        {
+            location: { lat: 50.6212, lng: -2.2768 },
+            content: '<div id="content">' +
                 '<div id="siteNotice">' +
                 "</div>" +
                 '<h2 id="firstHeading" class="firstHeading">Durdle Door</h2>' +
                 '<div id="bodyContent">' +
-                '<p><b>Durdle Door</b>, is a natural limestone arch on the Jurassic Coast in Dorset, England.' +
-                'Walking this beautiful part of the <b>Jurassic Coast</b> an (UNESCO World Heritage Site)' +
+                '<p><b>Durdle Door</b>, is a natural limestone arch on the Jurassic Coast in Dorset, England.</p>' +
+                '<h3><i class="fas fa-walking"></i></h3>' +
+                '<p>This beautiful part of the <b>Jurassic Coast</b> an (UNESCO World Heritage Site)' +
                 'As a family we like to start from the Lulworth Cove car park (charges apply), making our way up the challenging gravel track to the site.' +
-                'This way you see the stunning scenery of Lulworth Cove, Man o War Bay and Durdle Door itself' +
-                'But there is also a car park a shorter walk away from Durdle Door for less of a challenge but still get to sample stunning site. </p>' +
+                ' This way you see the stunning scenery of Lulworth Cove, Man o War Bay and Durdle Door itself' +
+                ' There is also a car park which a shorter walk away from Durdle Door for less of a challenge but still get to sample this stunning site. </p>' +
                 '<img src="assets/images/durdledoor.jpg" width=180 height=80>' +
                 "</div>" +
                 "</div>"
-    });
-    google.maps.event.addListener(marker1, 'click', function() {
-        infoWindow1.open(map, marker1);
-    });
-
-    const marker2 = new google.maps.Marker({
-          position: new google.maps.LatLng(50.6395, -2.0566),
-          map: map,
-          title: 'Corfe Castle'
-    });
-    
-    const infoWindow2 = new google.maps.InfoWindow({
-        content: '<div id="content">' +
+        },
+        {
+            location: { lat: 50.6395, lng: -2.0566 },
+            content: '<div id="content">' +
                 '<div id="siteNotice">' +
                 "</div>" +
                 '<h2 id="firstHeading" class="firstHeading">Corfe Castle</h2>' +
                 '<div id="bodyContent">' +
-                '<p><b>Corfe Castle</b>, is a castle ruins standing above the village of the same name on the Isle of Purbeck in the county of Dorset, England.' +
-                'Owned by the National Trust, the castle is open to the public </p>' +
+                '<p><b>Corfe Castle</b>, is a castle ruins standing above the village of the same name on the Isle of Purbeck in the county of Dorset, England.</p>' +
+                '<h3><i class="fas fa-walking"></i></h3>' +
+                '<p>Owned by the National Trust, the castle is open to the public </p>' +
                 '<img src="assets/images/corfe.jpeg" width=180 height=80>' +
                 "</div>" +
                 "</div>"
-    });
-
-    google.maps.event.addListener(marker2, 'click', function() {
-        infoWindow2.open(map, marker2);
-    });
-
-const marker3 = new google.maps.Marker({
-          position: new google.maps.LatLng(51.1789, -1.8262),
-          map: map,
-          title: 'Stonehenge'
-    });
-    
-    const infoWindow3 = new google.maps.InfoWindow({
-        content: '<div id="content">' +
+        },
+        {
+            location: { lat: 51.1789, lng: -1.8262 },
+            content: '<div id="content">' +
                 '<div id="siteNotice">' +
                 "</div>" +
                 '<h2 id="firstHeading" class="firstHeading">Stonehenge</h2>' +
                 '<div id="bodyContent">' +
-                '<p><b>Stonehenge</b>, is a prehistoric monument on Salisbury Plain in Wiltshire, England.' +
-                'One of the most famous landmarks in the UK, Stonehenge is regarded as a British cultural icon.' +
+                '<p><b>Stonehenge</b>, is a prehistoric monument on Salisbury Plain in Wiltshire, England.</p>' +
+                '<h3><i class="fas fa-walking"></i></h3>' +
+                '<p>Stonehenge is a great walk for adults and children alike!' + 
+                'If you would like to focus of a walk around the wonderful countryside,' +
+                'we suggest that you park up on the gravel track not far from the stones, One of the most famous landmarks in the UK, Stonehenge is regarded as a British cultural icon.' +
                 'Stonehenge is owned by the Crown and managed by English Heritage; the surrounding land is owned by the National Trust. </p>' +
                 '<img src="assets/images/stonehenge.jpeg" width=180 height=80>' +
                 "</div>" +
                 "</div>"
-    });
-
-    google.maps.event.addListener(marker3, 'click', function() {
-        infoWindow3.open(map, marker3);
-    });
-
-
-    const marker4 = new google.maps.Marker({
-          position: new google.maps.LatLng(50.8764, -1.6312),
-          map: map,
-          title: 'New Forest'
-    });
-    
-    const infoWindow4 = new google.maps.InfoWindow({
-        content: '<div id="content">' +
+        },
+        {
+            location: { lat: 50.8764, lng: -1.6312 },
+            content: '<div id="content">' +
                 '<div id="siteNotice">' +
                 "</div>" +
                 '<h2 id="firstHeading" class="firstHeading">New Forest National Park</h2>' +
                 '<div id="bodyContent">' +
                 '<p>The <b>New Forest</b>, is one of the largest remaining tracts of unenclosed pasture land,' +
-                'heathland and forest in Southern England, covering southwest Hampshire and southeast Wiltshire.' +
-                'More to write. </p>' +
+                'heathland and forest in Southern England, covering southwest Hampshire and southeast Wiltshire.</p>' +
+                '<h3><i class="fas fa-walking"></i></h3>' +
+                '<p>More to write. </p>' +
                 '<img src="assets/images/hampshire.jpeg" width=180 height=80>' +
                 "</div>" +
                 "</div>"
-    });
+        },
+        {
+            location: { lat: 50.7116, lng: -2.7637 },
+            content: '<div id="content">' +
+                '<div id="siteNotice">' +
+                "</div>" +
+                '<h2 id="firstHeading" class="firstHeading">West Bay</h2>' +
+                '<div id="bodyContent">' +
+                '<p><b>West Bay</b>, West Bay, also known as Bridport Harbour,' +
+                'is a small harbour settlement and resort on the English Channel coast in Dorset.</p>' +
+                '<h3><i class="fas fa-walking"></i></h3>' +
+                '<p>More to write. </p>' +
+                '<img src="assets/images/westbay.jpeg" width=180 height=80>' +
+                "</div>" +
+                "</div>"
+        }
+    ];
 
-    google.maps.event.addListener(marker4, 'click', function() {
-        infoWindow4.open(map, marker4);
-    });
+    // Loop through markers
+    for (let i = 0; i < markers.length; i++) {
+        // Add marker
+        addMarker(markers[i]);
+    }
+
+    // Add Marker
+
+    function addMarker(property){
+
+        const marker = new google.maps.Marker({
+            position:property.location,
+            map:map,
+            });
+
+        // Check content
+        if(property.content){
+
+            const infWin = new google.maps.InfoWindow({
+            content: property.content
+
+            });
+
+            marker.addListener("click", () => {
+                infWin.open(map, marker);
+            })
+    
+        }
+    }
 }
